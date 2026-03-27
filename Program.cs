52ed3d7e507eddb8c3d554052716fce88a144c38
Nourhan-elmahdy <120050771+Nourhan-elmahdy@users.noreply.github.com>
2026-03-27 23:05:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace task1_OOP_Eraaasoft
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students =  new List<Student>();
+
+            Student s1 = new Student();
+            Student s2 = new Student();
+
+
+            s1.name = "Ahmed";
+            s1.id = 1;
+            s1.studentLevel = StudentLevel.Senior; 
+            s1.Addr = new Address();
+
+            
+            s1.Addr.Street = "Cairo";
+            s1.Addr.City = "Nasr City";
+            s1.Addr.ZipCode = "12345";
+            students.Add(s1);
+
+            s2.name = "Mai";
+            s2.id = 2;
+            s2.studentLevel = StudentLevel.Junior;
+            students.Add(s2);
+            s2.Addr = new Address();
+            s2.Addr.Street = "Giza";
+            s2.Addr.City = "Dokki";
+            s2.Addr.ZipCode = "54321";
+
+            s1.DisplayInfo();
+            Console.WriteLine("\n");
+            s2.DisplayInfo();
+
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_OOP_Eraaasoft
+{
+    internal class Student
+    {
+        public int id;
+        public string name;
+        public StudentLevel studentLevel;
+        public Address Addr;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Student ID: {id}");
+            Console.WriteLine($"Student Name: {name}");
+            Console.WriteLine($"Student Address: {Addr.City} {Addr.Street} {Addr.ZipCode}");
+            Console.WriteLine($"Student Level: {studentLevel}");
+        }
+    }
+
+}
